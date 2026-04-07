4404abfc6eaaa3d1ee9be293cb0d24c080e9cdd0
GGN_2015 <neko@jlulug.org>
2026-04-07 13:31:35 +0800

    v2026-04-07

--- create_aimtool_file/main.py
+++ create_aimtool_file/main.py
@@ -1,5 +1,6 @@
 from solve_rigid_point_set_rt import compute_best_rigid_transform
 from solve_rigid_point_set_rt import apply_transform
+from fit_3d_plane             import fit_3d_plane
 import numpy as np
 import os
 
@@ -32,10 +33,7 @@ def unit(vec:np.ndarray):
     return vec / np.linalg.norm(vec)
 
 # Compute tool file data
-def calculate_tool_file_data(node_set:np.ndarray, dtype=np.float32, iter_time:int=2):
-    if iter_time <= 0:
-        return node_set
-    
+def calculate_tool_file_data(node_set:np.ndarray, dtype=np.float32, according_to_manual:bool=False):
     if len(node_set.shape) != 2: # Must be a 2D array
         raise ValueError()
     if node_set.shape[1] != 3: # Must be 3D points
@@ -47,11 +45,23 @@ def calculate_tool_file_data(node_set:np.ndarray, dtype=np.float32, iter_time:in
     node_set = node_set.astype(dtype)
     
     # Sort descending by distance from the origin
-    arr = [
-        (float(-np.linalg.norm(node_set[i])), node_set[i])
-        for i in range(node_set.shape[0])
-    ]
-    arr.sort()
+    if not according_to_manual:
+        arr = [
+            (-np.linalg.norm(node_set[i]), node_set[i])
+            for i in range(node_set.shape[0])
+        ]
+        arr.sort()
+
+    # Sort descending by abs(Y) from less to more
+    else:
+        arr = [
+            ((
+                float(node_set[i][1]), # Y
+                float(node_set[i][0])  # X
+            ), node_set[i])
+            for i in range(node_set.shape[0])
+        ]
+        arr.sort()
 
     # Get sorted numpy array
     np_arr = np.array([
@@ -59,12 +69,28 @@ def calculate_tool_file_data(node_set:np.ndarray, dtype=np.float32, iter_time:in
     ]).astype(dtype)
 
     # Translate centroid to origin
-    np_move = (np_arr - np_arr.mean(axis=0)).astype(dtype)
+    raw_centroid = np_arr.mean(axis=0)
+    np_move = (np_arr - raw_centroid).astype(dtype)
 
     # Compute current X, Y, Z axes
     x_axis = unit(np_move[0])
     y_axis = unit(np_move[1])
-    z_axis = unit(cross_3d(x_axis, y_axis).astype(dtype))
+
+    if not according_to_manual:
+        # Test the direction of z
+        z_test_axis = unit(cross_3d(x_axis, y_axis).astype(dtype))
+        z_axis = fit_3d_plane(np_move)[0]
+        if np.dot(z_axis, z_test_axis) < 0:
+            z_axis = -z_axis
+        z_axis = unit(z_axis)
+
+    else:
+        z_axis = fit_3d_plane(np_move)[0]
+        if np.dot(raw_centroid, z_axis) > 0: # Make z_axis inner
+            z_axis = -z_axis
+        z_axis = unit(z_axis)
+
+    # Caculate real_y, recalculate X
     y_axis = unit(cross_3d(z_axis, x_axis).astype(dtype))
     x_axis = unit(cross_3d(y_axis, z_axis).astype(dtype))
 
@@ -92,14 +118,16 @@ def calculate_tool_file_data(node_set:np.ndarray, dtype=np.float32, iter_time:in
     return aim_exp
 
 # Create tool file
-def create_aimtool_file(aim_dir:str, tool_name:str, node_set:np.ndarray):
+def create_aimtool_file(aim_dir:str, tool_name:str, node_set:np.ndarray, according_to_manual:bool=False):
     aim_dir = os.path.abspath(aim_dir)
     if not os.path.isdir(aim_dir):
         raise FileNotFoundError(f"{aim_dir} is not a directory.")
     aim_path = os.path.join(aim_dir, f"{tool_name}.aimtool")
 
     # Compute tool file content
-    np_coord = calculate_tool_file_data(node_set)
+    np_coord = calculate_tool_file_data(
+        node_set, 
+        according_to_manual=according_to_manual)
 
     # Create tool file
     with open(aim_path, "w") as fp:
@@ -116,11 +144,11 @@ def create_aimtool_file(aim_dir:str, tool_name:str, node_set:np.ndarray):
 
 if __name__ == "__main__":
     P = np.array([
-        [-154.349,  70.446, 835.555], # 3rd closest to centroid
-        [-114.986,  70.007, 830.095], # Farthest from center
-        [-150.089,  99.024, 813.499], # Closest to centroid
-        [-129.166, 102.473, 807.977], # 2nd closest to centroid
+        [-154.349,  70.446, 835.555],
+        [-114.986,  70.007, 830.095],
+        [-150.089,  99.024, 813.499],
+        [-129.166, 102.473, 807.977],
     ])
 
     # Create tool file
-    create_aimtool_file(".", "BONE-1", P)
+    create_aimtool_file(".", "BONE-1", P, True)
